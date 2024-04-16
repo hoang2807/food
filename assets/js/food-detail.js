4a683d74d7c079783a3aa85806442b8a9abe38cd
hoang2807 <viethoang28072000@gmail.com>
2024-04-16 11:34:52 +0700

    feat: 🎸 slider food detail

--- assets/js/food-detail.js
+++ assets/js/food-detail.js
@@ -3,3 +3,36 @@ function changeImage(image) {
   bigImage.src = image.src
 }
 
+const slideBlogs_detail =
+  new Swiper(".related-food .swiper", {
+    direction: "horizontal",
+    slidesPerView: 3,
+    spaceBetween: 38,
+    speed: 1000,
+    watchOverflow: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      200: {
+        slidesPerView: 1,
+        spaceBetween: 16,
+      },
+      480: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 38,
+      },
+    },
+    navigation: {
+      nextEl: ".related-food .swiper-button-next",
+      prevEl: ".related-food .swiper-button-prev",
+    },
+  }) || false;
